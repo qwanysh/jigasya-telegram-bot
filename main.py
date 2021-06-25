6d96720eb338e3b6375091d1acfd10b7573e6d84
qwanysh <olimpsdu@gmail.com>
2021-06-25 15:34:17 +0600

    Start webhook when executed in heroku

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from src import bot
+from src import bot, config
 
-bot.start_polling()
-bot.idle()
+WEBBOOK_URL_TEMPLATE = 'https://{}.herokuapp.com/{}'
+
+
+def main():
+    if config.HEROKU_APP_NAME:
+        bot.start_webhook(
+            listen='0.0.0.0',
+            port=config.PORT,
+            url_path=config.TOKEN,
+            webhook_url=WEBBOOK_URL_TEMPLATE.format(
+                config.HEROKU_APP_NAME, config.TOKEN,
+            ),
+        )
+        bot.idle()
+    else:
+        bot.start_polling()
+
+
+if __name__ == '__main__':
+    main()
--- src/config.py
+++ src/config.py
@@ -4,4 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+PORT = os.getenv('PORT')
+
 TOKEN = os.getenv('TOKEN')
