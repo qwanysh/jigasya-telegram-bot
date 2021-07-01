0a25a0ff16577b82c037bc6ab618679013dee07a
qwanysh <olimpsdu@gmail.com>
2021-07-01 21:50:49 +0600

    Beautify code

--- src/config.py
+++ src/config.py
@@ -11,6 +11,6 @@ SUPERUSER_ID = int(os.getenv('SUPERUSER_ID'))
 JIGASYA_CHAT_ID = int(os.getenv('JIGASYA_CHAT_ID'))
 
 DATABASE_URL = os.getenv('DATABASE_URL', '')
-DATABASE_URL = DATABASE_URL.replace(
-    'postgres', 'postgresql', 1,  # sqlalchemy doesn't support postgres driver
-) if DATABASE_URL.startswith('postgres://') else DATABASE_URL
+# sqlalchemy doesn't support postgres driver
+if DATABASE_URL.startswith('postgres://'):
+    DATABASE_URL = DATABASE_URL.replace('postgres', 'postgresql', 1)
