6989de89e9f969c7039523a7b6061f7986b947d2
qwanysh <olimpsdu@gmail.com>
2021-08-02 17:38:14 +0600

    Add /members command handler

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,3 +1,4 @@
 from .command_handlers import (chat_info_handler, find_dolbaeb_handler,
-                               register_conversation_handler, start_handler)
+                               members_handler, register_conversation_handler,
+                               start_handler)
 from .scheduled_handlers import bugin_ne_plan_handler
--- src/handlers/command_handlers/general.py
+++ src/handlers/command_handlers/general.py
@@ -1,7 +1,8 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 
-from src.utils import permissions
+from src import database, models
+from src.utils import helpers, permissions
 
 
 def start_handler(update: Update, context: CallbackContext):
@@ -11,3 +12,13 @@ def start_handler(update: Update, context: CallbackContext):
 @permissions.superuser_only
 def chat_info_handler(update: Update, context: CallbackContext):
     update.message.reply_text(update.to_json())
+
+
+@permissions.jigasya_chat_only
+def members_handler(update: Update, context: CallbackContext):
+    with database.Session() as session:
+        members = session.query(models.JigasyaMember).order_by(
+            models.JigasyaMember.created_at.desc(),
+        )
+    message = helpers.render_message('members.html', members=members)
+    update.message.reply_html(message, disable_notification=True)
