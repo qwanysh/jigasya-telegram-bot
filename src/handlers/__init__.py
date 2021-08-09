24a818b43a3afe292d2ebd91d53268b1f6265c40
qwanysh <olimpsdu@gmail.com>
2021-08-09 16:31:06 +0600

    Add register_handler

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,4 +1,5 @@
 from .command_handlers import (chat_info_handler, find_dolbaeb_handler,
                                members_handler, register_conversation_handler,
                                start_handler)
+from .middleware_handlers import register_handler
 from .scheduled_handlers import bugin_ne_plan_handler
---
+++ src/handlers/middleware_handlers.py
@@ -0,0 +1,34 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+
+from src import consts, database, models
+from src.redis_client import redis_client
+
+
+def register_handler(update: Update, context: CallbackContext):
+    from_user = update.message.from_user
+    redis_key = f'registered_member:{from_user.id}'
+
+    if redis_client.get(redis_key):
+        update.message.reply_text('user already in cache')
+        return
+
+    with database.Session() as session:
+        member = session.query(models.JigasyaMember).get(from_user.id)
+
+    if member:
+        member.username = from_user.username
+        member.first_name = from_user.first_name
+        member.last_name = from_user.last_name
+
+        with database.Session() as session, session.begin():
+            session.add(member)
+    else:
+        member = models.JigasyaMember(
+            telegram_id=from_user.id, username=from_user.username,
+            first_name=from_user.first_name, last_name=from_user.last_name,
+        )
+        with database.Session() as session, session.begin():
+            session.add(member)
+
+    redis_client.setex(redis_key, consts.REGISTER_CACHE_TTL, from_user.id)
