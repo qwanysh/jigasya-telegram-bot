18b0f0309408cb403c603c5c218f0cec236175ee
qwanysh <olimpsdu@gmail.com>
2021-08-09 16:31:40 +0600

    Add get_middleware_handler func

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,6 +1,12 @@
+from telegram.ext import Filters, MessageHandler
+
 from src import config
 
 
 def render_message(template_name, *args, **kwargs):
     template = config.jinja2_env.get_template(template_name)
     return template.render(*args, **kwargs)
+
+
+def get_middleware_handler(handler):
+    return MessageHandler(Filters.all, handler)
